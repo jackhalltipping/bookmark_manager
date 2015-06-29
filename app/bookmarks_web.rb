5c3a07f031ff38bce4d25eb9bc493a94f84eefd6
Simon Legg <leggsimon@gmail.com>
2015-06-29 16:56:34 +0100

    Added links.erb

--- app/bookmarks_web.rb
+++ app/bookmarks_web.rb
@@ -1,8 +1,15 @@
 require 'sinatra/base'
 
 class BookmarksWeb < Sinatra::Base
+
+  set :views, proc { File.join(root, '..', 'views') }
+
   get '/' do
     'Hello BookmarksWeb!'
+  end
+
+  get '/links' do
+    erb :links
   end
 
   # start the server if ruby file executed directly
