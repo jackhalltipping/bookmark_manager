0974186effc1bc6998dc0d941cb43fa7889637c2
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-06-30 12:08:30 +0100

    removed heroku config

--- app/data_mapper_setup.rb
+++ app/data_mapper_setup.rb
@@ -3,9 +3,7 @@ require './app/models/link.rb'
 
 env = ENV['RACK_ENV'] || 'development'
 
-#DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{env}")
+DataMapper.setup(:default, "postgres://localhost/bookmark_manager_#{env}")
 
 DataMapper.finalize
 
