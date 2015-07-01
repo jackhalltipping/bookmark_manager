355216c0a1abe93ec74b228be8ada5761e52b439
Faisal Choura <faisal.choura@gmail.com>
2015-07-01 11:55:55 +0100

    adding users with failing test

--- app/app.rb
+++ app/app.rb
@@ -4,6 +4,10 @@ require 'data_mapper'
 require './app/data_mapper_setup.rb'
 
 class App < Sinatra::Base
+
+  enable :sessions
+  set :session_secret, 'super secret'
+
   get '/links' do
     @links = Link.all
     erb :index
@@ -27,5 +31,20 @@ class App < Sinatra::Base
     tag = Tag.first(name: params[:name])
     @links = tag ? tag.links : []
     erb :index
+  end
+
+  get '/users/new' do
+    erb :'users/new'
+  end
+
+  post '/users' do
+    session[:user] = User.create(email: params[:email],
+                password: params[:password])
+    session[:user_id] = user.id
+    redirect('/')
+  end
+
+  def current_user
+    session[:user]
   end
 end
