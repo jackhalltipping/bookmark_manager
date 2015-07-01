015efb1982a24751e5c05911a73ffd1ae4129f09
Jack Hall-Tipping <jack.halltipping@gmail.com>
2015-07-01 15:19:22 +0100

    password confirmation working

--- app/app.rb
+++ app/app.rb
@@ -43,7 +43,8 @@ class App < Sinatra::Base
 
   post '/users' do
     user = User.create(email: params[:email],
-                password: params[:password])
+                password: params[:password],
+                password_confirmation: params[:password_confirmation])
     session[:user_id] = user.id
     redirect('/')
   end
