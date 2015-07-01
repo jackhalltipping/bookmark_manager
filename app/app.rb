bef10ebe9396ba3b0dd604b6bce97dc4c5679b97
Faisal Choura <faisal.choura@gmail.com>
2015-07-01 14:25:07 +0100

    after sign up user gets a welcome message

--- app/app.rb
+++ app/app.rb
@@ -8,6 +8,10 @@ class App < Sinatra::Base
   enable :sessions
   set :session_secret, 'super secret'
 
+  get '/' do
+    redirect 'links'
+  end
+
   get '/links' do
     @links = Link.all
     erb :index
@@ -38,13 +42,13 @@ class App < Sinatra::Base
   end
 
   post '/users' do
-    session[:user] = User.create(email: params[:email],
+    user = User.create(email: params[:email],
                 password: params[:password])
     session[:user_id] = user.id
     redirect('/')
   end
 
   def current_user
-    session[:user]
+    User.get(session[:user_id])
   end
 end
